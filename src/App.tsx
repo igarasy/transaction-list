dc19a5d97fbdaa3304c4db4c82c435880a06c6b0
igarasy <lucas.igarashi@caju.com.br>
2022-08-30 22:24:41 -0300

    refactor: add css reset

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,19 @@ import Transaction from './components/pages/TransactionsList/index'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import CreateTransaction from './components/pages/CreateTransaction/index'
 import { SnackbarProvider } from 'notistack'
+import { createGlobalStyle } from 'styled-components'
 
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+`
 function App() {
   return (
     <>
+    <GlobalStyle/>
       <SnackbarProvider>
         <BrowserRouter>
           <Routes>
